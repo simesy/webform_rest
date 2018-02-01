0a91b0ed24c75a601a598a786076fdd48062e538
Si Hobbs <simon@hobbs.id.au>
2018-02-01 17:44:16 +1100

    Displays the drupal form.

--- example/webform_vue/src/components/HelloWorld.vue
+++ example/webform_vue/src/components/HelloWorld.vue
@@ -10,64 +10,26 @@
 
 import Vue from "vue";
 import VueFormGenerator from "vue-form-generator";
+import axios from 'axios';
+
 Vue.use(VueFormGenerator);
 
 export default {
   data () {
     return {
-      model: {
-        id: 1,
-        name: "John Doe",
-        password: "J0hnD03!x4",
-        skills: ["Javascript", "VueJS"],
-        email: "john.doe@gmail.com",
-        status: true
-      },
-
-      schema: {
-        fields: [
-          {
-            type: "input",
-            inputType: "text",
-            label: "ID (disabled text field)",
-            model: "id",
-            readonly: true,
-            disabled: true
-          },
-          {
-            type: "input",
-            inputType: "text",
-            label: "Name",
-            model: "name",
-            id: "user_name",
-            placeholder: "Your name",
-            featured: true,
-            required: true
-          },
-          {
-            type: "input",
-            inputType: "email",
-            label: "E-mail",
-            model: "email",
-            placeholder: "User's e-mail address"
-          },
-          {
-            type: "input",
-            inputType: "password",
-            label: "Password",
-            model: "password",
-            min: 6,
-            required: true,
-            hint: "Minimum 6 characters",
-          }
-        ]
-      },
-      formOptions: {
-        validateAfterLoad: true,
-        validateAfterChanged: true,
-        fieldIdPrefix: 'user-'
-      }
+      model: {},
+      schema: {},
+      formOptions: {}
     }
+  },
+
+  created() {
+    axios.get('http://CHANGEME/webform_vue/contact/elements?_format=json')
+    .then(response => {
+      this.model = response.data.model;
+      this.schema = response.data.schema;
+      this.formOptions = response.data.formOptions;
+    });
   }
 }
 </script>
